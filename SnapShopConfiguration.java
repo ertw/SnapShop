966682bb385f1b8cb200525d33e3055845a2deb5
Erik Williamson <erik.williamson@applydigital.com>
2023-11-12 21:37:23 -0800

    Lint and update default filepath

--- SnapShopConfiguration.java
+++ SnapShopConfiguration.java
@@ -1,4 +1,4 @@
-// Write your short report here (-2 if there is no report)
+// TODO Write your short report here (-2 if there is no report)
 
 /**
  * A class to configure the SnapShop application
@@ -6,19 +6,19 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class SnapShopConfiguration
-{
-  /**
-   * Method to configure the SnapShop.  Call methods like addFilter
-   * and setDefaultFilename here.
-   * @param theShop A pointer to the application
-   */
-  public static void configure(SnapShop theShop)
-  {
+public class SnapShopConfiguration {
+    /**
+     * Method to configure the SnapShop.  Call methods like addFilter
+     * and setDefaultFilename here.
+     *
+     * @param theShop A pointer to the application
+     */
+    public static void configure(SnapShop theShop) {
 
-    theShop.setDefaultFilename("c:/users/cd/desktop/jpegs/billg.jpg");
-    theShop.addFilter(new FlipHorizontalFilter(), "Flip Horizontal");
-
-    // add your other filters below
-  }
+//    theShop.setDefaultFilename("c:/users/cd/desktop/jpegs/billg.jpg");
+        theShop.setDefaultFilename("program_9/billg.jpg");
+        theShop.addFilter(new FlipHorizontalFilter(), "Flip Horizontal");
+        theShop.addFilter(new FlipVerticalFilter(), "Flip Vertical");
+//        theShop.addFilter(new GaussianBlurFilter(), "Gaussian Blur");
+    }
 }
