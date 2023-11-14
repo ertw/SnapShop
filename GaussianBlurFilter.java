52b1779f7ecf47ebcc01a3c22239178841604554
Erik Williamson <erik.williamson@applydigital.com>
2023-11-13 20:41:09 -0800

    Factor out the kernal into it's own class

--- GaussianBlurFilter.java
+++ GaussianBlurFilter.java
@@ -1,53 +1,13 @@
 /**
  * Filter that applies a Gaussian blur to an image.
- * Weights:
- * 1 2 1
- * 2 4 2
- * 1 2 1
  */
 public class GaussianBlurFilter implements Filter {
-    private static Pixel convolve(int row, int col, Pixel[][] data) {
-        final int[][] kernel = {
+    public void filter(PixelImage pixelImage) {
+        final int[][] gaussianKernel = {
                 {1, 2, 1},
                 {2, 4, 2},
-                {1, 2, 1}
-        };
-        final int kernelSum = 16;
-        Pixel NW = data[row - 1][col - 1];
-        Pixel N = data[row - 1][col];
-        Pixel NE = data[row - 1][col + 1];
-        Pixel W = data[row][col - 1];
-        Pixel C = data[row][col];
-        Pixel E = data[row][col + 1];
-        Pixel SW = data[row + 1][col - 1];
-        Pixel S = data[row + 1][col];
-        Pixel SE = data[row + 1][col + 1];
-        final int newRed = (NW.red * 1 + N.red * 2 + NE.red * 1 +
-                W.red * 2 + C.red * 4 + E.red * 2 +
-                SW.red * 1 + S.red * 2 + SE.red * 1) / kernelSum;
-        final int newGreen = (NW.green * 1 + N.green * 2 + NE.green * 1 +
-                W.green * 2 + C.green * 4 + E.green * 2 +
-                SW.green * 1 + S.green * 2 + SE.green * 1) / kernelSum;
-        final int newBlue = (NW.blue * 1 + N.blue * 2 + NE.blue * 1 +
-                W.blue * 2 + C.blue * 4 + E.blue * 2 +
-                SW.blue * 1 + S.blue * 2 + SE.blue * 1) / kernelSum;
-        return new Pixel(newRed, newGreen, newBlue);
-    }
-
-    public void filter(PixelImage image) {
-        Pixel[][] data = image.getData();
-        final int height = image.getHeight();
-        final int width = image.getWidth();
-        final int kernelSize = 3;
-        final int kernelCenter = kernelSize / 2;
-
-        for (int row = 20; row < height - 20; row++) {
-            for (int col = 20; col < width - 20; col++) {
-                Pixel currentPixel = data[row][col];
-                Pixel newPixel = convolve(row, col, data);
-                data[row][col] = newPixel;
-            }
-        }
-        image.setData(data);
+                {1, 2, 1}};
+        Kernel kernel = new Kernel(gaussianKernel);
+        kernel.applyTo(pixelImage);
     }
 }
---
+++ Kernel.java
@@ -0,0 +1,98 @@
+/**
+ * Kernel class for applying a convolution matrix to an image.
+ */
+public class Kernel {
+    /**
+     * The matrix to apply to the image.
+     */
+    private final int[][] matrix;
+    /**
+     * The width of the matrix.
+     */
+    private final int width;
+    /**
+     * The height of the matrix.
+     */
+    private final int height;
+    /**
+     * The sum of all the values in the matrix.
+     * This is used to normalize the resulting pixel.
+     */
+    private final int sum;
+
+    /**
+     * Create a new kernel with the given matrix.
+     *
+     * @param matrix The convolution matrix to apply to the image.
+     */
+    public Kernel(int[][] matrix) {
+        this.matrix = matrix;
+        this.width = matrix.length;
+        this.height = matrix[0].length;
+        this.sum = calculateSum(matrix);
+    }
+
+    /**
+     * Calculate the sum of all the values in the matrix.
+     *
+     * @param matrix The convolution matrix to calculate the sum of.
+     * @return The sum of all the values in the matrix.
+     */
+    private int calculateSum(int[][] matrix) {
+        int total = 0;
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                total += anInt;
+            }
+        }
+        return total;
+    }
+
+    /**
+     * Apply the convolution matrix to the given pixel.
+     *
+     * @param row  The row of the pixel to apply the matrix to.
+     * @param col  The column of the pixel to apply the matrix to.
+     * @param data The image data to apply the matrix to.
+     * @return The new pixel after applying the matrix.
+     */
+    private Pixel convolve(int row, int col, Pixel[][] data) {
+        Pixel newPixel = new Pixel(0, 0, 0);
+        for (int mx = 0; mx < width; mx++) {
+            for (int my = 0; my < height; my++) {
+                int imageX = row + mx - width / 2;
+                int imageY = col + my - height / 2;
+                if (imageX >= 0 && imageX < data.length && imageY >= 0 && imageY < data[0].length) {
+                    Pixel pixel = data[imageX][imageY];
+                    int kernelValue = matrix[mx][my];
+                    newPixel.red += pixel.red * kernelValue;
+                    newPixel.green += pixel.green * kernelValue;
+                    newPixel.blue += pixel.blue * kernelValue;
+                }
+            }
+        }
+        newPixel.red /= sum;
+        newPixel.green /= sum;
+        newPixel.blue /= sum;
+        return newPixel;
+    }
+
+    /**
+     * Apply the convolution matrix to the given image.
+     *
+     * @param pixelImage The image to apply the matrix to.
+     */
+    public void applyTo(PixelImage pixelImage) {
+        Pixel[][] data = pixelImage.getData();
+        final int imageHeight = pixelImage.getHeight();
+        final int imageWidth = pixelImage.getWidth();
+        final int offset = Math.max(width, height) / 2; // prevent out of bounds
+        for (int row = offset; row < imageHeight - offset; row++) {
+            for (int col = offset; col < imageWidth - offset; col++) {
+                Pixel newPixel = convolve(row, col, data);
+                data[row][col] = newPixel;
+            }
+        }
+        pixelImage.setData(data);
+    }
+}
