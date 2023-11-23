1820fbe3b79bddb559d5b84b45b1b4543ccd8ee0
Erik Williamson <erik.williamson@applydigital.com>
2023-11-23 10:25:27 -0800

    add project notes

--- SnapShopConfiguration.java
+++ SnapShopConfiguration.java
@@ -5,7 +5,14 @@
  * - Created a Kernel class to represent and apply a convolution matrix. This allows me to easily create new matrix style filters by defining a matrix.
  * - Added Pixel.normalize(Consumer<Pixel> consumer) to Pixel.java, which accepts a function that takes a Pixel and potentially modifies it.
  * - Added PixelImage.transform(Consumer<Pixel> consumer) to PixelImage.java, which allows me to define a function to apply to each pixel in an entire image.
- * Beyond the basic filters, I also added a quantize filter, which I think looks pretty cool.
+ * - I support arbitrarily sized convolution matrices, but I've only tested through 5x5. I didn't find any interesting effects with larger convolutions, so I've only used 3x3 matrices.
+ * Beyond the basic filters, I added a few more:
+ * - QuantizeFilter: Quantiles the image to a given number of colors.
+ * - EmbossFilter: Applies an emboss effect to the image.
+ * The emboss filter was the most difficult one for me to implement, because I initially tried embossing with just the convolution weights. I eventually discovered
+ * that I needed to apply a bias to each pixel transformation. Fortunately, Java's method overloading allowed me to create a Kernel constructor that accepts a bias.
+ * Because the filters compose, I was able to pass the emboss filter a greyscale filter, which results in a final image that is identical to that produced in common
+ * image manipulation programs.
  */
 
 /**
