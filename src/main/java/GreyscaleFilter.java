da5ddac65c0cc1d59e6c2694911eb3d86b975eca
Erik Williamson <erik.williamson@applydigital.com>
2023-11-29 20:20:17 -0800

    Use weighted average for greyscale

--- src/main/java/GreyscaleFilter.java
+++ src/main/java/GreyscaleFilter.java
@@ -4,11 +4,10 @@
 public class GreyscaleFilter implements Filter {
     public void filter(PixelImage pixelImage) {
         pixelImage.transform((p) -> {
-            int average = (p.red + p.green + p.blue) / 3;
-//            int weightedAverage = (int) (p.red * 0.3) + (int) (p.green * 0.59) + (int) (p.blue * 0.11) / 3;
-            p.red = average;
-            p.green = average;
-            p.blue = average;
+            int weightedAverage = (int) (p.red * 0.3) + (int) (p.green * 0.59) + (int) (p.blue * 0.11) / 3; // values chosen to match human perception of color
+            p.red = weightedAverage;
+            p.green = weightedAverage;
+            p.blue = weightedAverage;
         });
     }
 }
